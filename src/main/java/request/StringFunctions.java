20eee2dba95640b57661d7da6fa2daf37a4a6d99
ClepaBoop <neondertalez0@gmail.com>
2021-05-04 20:27:02 +0700

    add newRequest

---
+++ src/main/java/request/StringFunctions.java
@@ -0,0 +1,27 @@
+package request;
+
+public class StringFunctions {
+
+    public static String cCONCAT_1_9 = "select \n" +
+            "  concat(last_name, ' ', first_name, ' ', middle_name, ';') \n" +
+            "  as full_name\n" +
+            "from learndb2.employee\n" +
+            "order by full_name";
+
+    public static String _2_9 = "";
+
+    public static String _3_9 = "";
+
+    public static String _4_9 = "";
+
+    public static String _5_9 = "";
+
+    public static String _6_9 = "";
+
+    public static String _7_9 = "";
+
+    public static String _8_9 = "";
+
+    public static String _9_9 = "";
+
+}
